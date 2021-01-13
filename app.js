4b72f4127cafc53f8b1de80eefa99f0fbe2cce83
rawat-aakash <rawataakash70@gmail.com>
2021-01-13 21:01:12 +0530

    Slider fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let btn = document.getElementById("my-btn");
 let div = document.getElementById("hidden-div");
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
-const slides = document.querySelector(".slide");
+const slides = document.querySelectorAll(".slide");
 let index = 0;
 
 btn.addEventListener("click", () => {
@@ -14,3 +14,31 @@ btn.addEventListener("click", () => {
     btn.innerHTML = "Show All Categories";
   }
 });
+
+function display(ind) {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[ind].style.display = "flex";
+}
+
+function nextSlide() {
+  console.log("next");
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  display(index);
+}
+
+function prevSlide() {
+  console.log("prev");
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  display(index);
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
